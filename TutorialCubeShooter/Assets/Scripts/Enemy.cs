7b0e7cdeee1a82b8f2a0fb2711b2d3e670f55807
recyx <fe.schulz@gmx.net>
2017-05-16 16:50:57 +0200

    game as folder
    folder

---
+++ TutorialCubeShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+public class Enemy : LivingEntity {
+
+	UnityEngine.AI.NavMeshAgent pathfinder;
+	Transform target;
+
+	protected override void Start () {
+		base.Start();
+		pathfinder = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		StartCoroutine (UpdatePath ());
+	}
+	
+	void Update () {
+
+	}
+
+	IEnumerator UpdatePath (){
+		float refreshRate = 0.25f;
+
+		while (target != null) {
+			Vector3 targetPosition = new Vector3 (target.position.x, 0, target.position.z);
+			if (!dead) {
+				pathfinder.SetDestination (targetPosition);
+			}
+			yield return new WaitForSeconds (refreshRate);
+		}
+	}
+}
---
+++ TutorialCubeShooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public LayerMask collisionMask;
+	public float deathTime;
+	float speed = 10;
+	float damage = 1;
+	float deathTimer;
+
+	public void setSpeed (float newSpeed) {
+		speed = newSpeed;
+	}
+
+	void Start () {
+		deathTimer = deathTime;
+	}
+	
+	void Update () {
+		if(deathTimer < 0) {
+			GameObject.Destroy (gameObject);
+		} else {
+			deathTimer -= Time.deltaTime;
+		}
+		
+		float moveDistance = speed * Time.deltaTime;
+		CheckCollisions (moveDistance);
+		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+
+	void CheckCollisions(float moveDistance) {
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)) {
+			OnHitObject (hit);
+		}
+	}
+
+	void OnHitObject(RaycastHit hit) {
+		IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
+		if (damageableObject != null) {
+			damageableObject.TakeHit (damage, hit);
+		}
+		GameObject.Destroy (gameObject);
+	}
+}
